24cdb8dc25e80b4f424dc2ec8a1d3c6e8abf97fc
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2023-08-18 19:44:25 -0300

    First test

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,36 @@
-/**
- * @file This is the entrypoint for your project.
- * If used as a node module, when someone runs
- * `import stuff from 'your-module'` (typescript)
- * or `const stuff = require('your-module')` (javascript)
- * whatever is exported here is what they'll get.
- * For small projects you could put all your code right in this file.
- */
+import { OpenAI } from "langchain/llms/openai";
+import { RetrievalQAChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import * as fs from "fs";
+import dotenv from "dotenv";
 
-export * from './lib/sample-module.js';
-export default undefined;
+async function main() {
+  dotenv.config();
+
+  const text = fs.readFileSync(
+    "./data/PLATAFORMA LA LIBERTAD AVANZA.txt",
+    "utf8",
+  );
+  console.log(`Loaded text with ${text.length} characters`);
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+  const docs = await textSplitter.createDocuments([text]);
+
+  // Create a vector store from the documents.
+  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+
+  // Initialize a retriever wrapper around the vector store
+  const vectorStoreRetriever = vectorStore.asRetriever();
+
+  // Create a chain that uses the OpenAI LLM and HNSWLib vector store.
+  const model = new OpenAI({});
+  const chain = RetrievalQAChain.fromLLM(model, vectorStoreRetriever);
+  const res = await chain.call({
+    query:
+      "Resumiendo, ¿Cuáles son las propuestas de Milei respecto a la salud?",
+  });
+  console.log({ res });
+}
+
+main();
