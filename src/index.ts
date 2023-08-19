55d7c6e51928a767bd47e840d0ddbc8bb7ce1916
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2023-08-19 00:54:14 -0300

    Using Faiss to retrieve data and answer questions

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
-import { OpenAI } from "langchain/llms/openai";
-import { RetrievalQAChain } from "langchain/chains";
-import { HNSWLib } from "langchain/vectorstores/hnswlib";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import * as fs from "fs";
+import { RetrievalQAChain } from 'langchain/chains';
 import dotenv from "dotenv";
+import { FaissStore } from "langchain/vectorstores/faiss";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAI } from "langchain/llms/openai";
 
 async function main() {
   dotenv.config();
 
-  const text = fs.readFileSync(
-    "./data/PLATAFORMA LA LIBERTAD AVANZA.txt",
-    "utf8",
+  // Create docs with a loader
+  const loader = new TextLoader("./data/PLATAFORMA LA LIBERTAD AVANZA.txt");
+  const docs = await loader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter();
+  const splittedDocs = await splitter.splitDocuments(docs);
+
+  // Load the docs into the vector store
+  const vectorStore = await FaissStore.fromDocuments(
+    splittedDocs,
+    new OpenAIEmbeddings(),
   );
-  console.log(`Loaded text with ${text.length} characters`);
-  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
-  const docs = await textSplitter.createDocuments([text]);
 
-  // Create a vector store from the documents.
-  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-
-  // Initialize a retriever wrapper around the vector store
   const vectorStoreRetriever = vectorStore.asRetriever();
 
   // Create a chain that uses the OpenAI LLM and HNSWLib vector store.
@@ -28,7 +29,7 @@ async function main() {
   const chain = RetrievalQAChain.fromLLM(model, vectorStoreRetriever);
   const res = await chain.call({
     query:
-      "Resumiendo, ¿Cuáles son las propuestas de Milei respecto a la salud?",
+      "¿Diría que Milei va a conservar la salud pública y mejorarla en calidad?",
   });
   console.log({ res });
 }
